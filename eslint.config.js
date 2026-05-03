91cef3a0d58644e812749bd1a1b9185757f59881
Vishal Somaraju <vishalsomaraju9@gmail.com>
2026-05-03 23:18:43 +0530

    eslint config fix

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,32 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import tsParser from '@typescript-eslint/parser'
 
-export default defineConfig([
-  globalIgnores(['dist', 'coverage']),
+export default tseslint.config(
+  { ignores: ['dist', 'coverage'] },
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
-      'no-unused-vars': [
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
         'error',
         {
           varsIgnorePattern: '^[A-Z_]|^motion$',
@@ -39,5 +43,5 @@ export default defineConfig([
     rules: {
       'react-refresh/only-export-components': 'off',
     },
-  },
-])
+  }
+)
